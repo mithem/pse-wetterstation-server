f5a643b91249e496d111c222a8aaeba68dfbcf98
mithem <41842729+mithem@users.noreply.github.com>
2021-12-21 15:30:53 +0100

    Update datenabanksetup.sql

--- datenabanksetup.sql
+++ datenabanksetup.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE IF NOT EXISTS wetterstation;
-CREATE TABLE IF NOT EXISTS messwerte (temperatur double, niederschlag int, sonnenstunden int, feuchtigkeit double, wind double, druck double, zeit TIMESTAMP);
+CREATE TABLE IF NOT EXISTS messwerte (temperatur double, niederschlag int, lichtstaerke double, feuchtigkeit double, wind double, druck double, zeit TIMESTAMP);
