d9cc89b7d1456f42397306f51f3163194e06cbd6
adcancer <94910280+adcancer@users.noreply.github.com>
2021-12-21 13:54:28 +0100

    Add files via upload

--- statistiken.php
+++ statistiken.php
@@ -1,5 +1,23 @@
+	<?php
+	/*require_once("CWerte.php");
+	require_once("DAOWerte.php");
+	
+
+	global $CONNECTED_DB;
+	if(!$CONNECTED_DB) {
+	  exit("Verbindungsfehler: ".mysqli_connect_error());
+	}
+
+	$meinWert = new CWerte();
+	$meineDB = new DAOWerte();
+	
+	$meinWert = $meineDB->getWerte($db);
+	*/
+	?>
 <!DOCTYPE html>
+<html lang="en">
 <html>
+
 <head>
 	<title>Statistiken</title>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.js"></script>
@@ -11,6 +29,15 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+	
+	<!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 	
 	<style>
 		html, body {
@@ -85,17 +112,17 @@
 
 <body>
 </div>
+	
 	<nav class="navbar">
 		<a class="title" href="/" >Wetter Lohmar</a>
-		<div class="spacer"></div>
 		<a class="btn btn-primary" href="/">Aktuelles Wetter</a>
-		<a class="btn btn-primary" href="/aendern.php">Statistik ändern</a>
+		<!--<a class="btn btn-primary" href="/aendern.php">Statistik ändern</a>-->
 	</nav>
 	<div class="root">
 		<div class="group-row">
 			<div class="settings-container">
-
-					<select class="form-select" name="Zeitraum[]">
+				 
+					<select class="form-select" name="Zeitraum[]" id="abc">
 						<option value="" disabled Select>Zeitraum auswählen</option>
 						<option value="heute" selected>Heute</option>
 						<option value="eineWoche">diese Woche</option>
@@ -103,7 +130,7 @@
 						<option value="einJahr">dieses Jahr</option>
 						<option value="beginn">seit Beginn der Messung</option>
 					</select>
-					<select class="form-select" name="Wert[]">
+					<select class="form-select" name="Wert[]" id="def">
 						<option value="" disabled Select>Typ auswählen</option>
 						<option value="temperatur" selected>Temperatur</option>
 						<option value="niederschlag">Niederschlag</option>
@@ -116,40 +143,44 @@
 					document.getElementById('def').value
 					Muss noch in Button eingefügt werden
 					-->
-					<input type="button" name="submitTyp2" value="Wählen" onClick="window.location.href='/statistiken.php?zeit=?wert='">
-
-				<?php
-					/*if(isset($_POST['submitTyp'])){
-						if(!empty($_POST['Wert'])) {
-							foreach($_POST['Wert'] as $selected){
-								echo '  ' . $selected;
-							} 
-						}
+					<script>
+					function changeURL(){
+						window.location.href="/statistiken.php?zeit=document.getElementById(\'abc\').value'&wert=document.getElementById('def').value"
 					}
-					if(isset($_POST['submitTyp'])){
-						if(!empty($_POST['Zeitraum'])) {
-							foreach($_POST['Zeitraum'] as $selected){
-								echo '  ' . $selected;
-							} 
-						}
-					}						
-				?>*/
+					</script>
+						<input type="button" name="submitTyp" value="Wählen" onClick="changeURL()">				
 			</div>
+			
 			<div class="chart-container">
 				<canvas id="chart"></canvas>
-				<!-- Area Chart -->
-                            <div class="card shadow mb-4">
+				<div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary"></h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Temperatur</h6>
                                 </div>
                                 <div class="card-body">
                                     <div class="chart-area">
                                         <canvas id="myAreaChart"></canvas>
-                                    </div>
+                                    </div>                             
                                 </div>
                             </div>
 			</div>
 		</div>
 	</div>
-	<script>initChart();</script>
+	<script>initChart()</script>
+	<!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
 </body>
+</html>
