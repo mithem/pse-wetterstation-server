fd0fe4aa660e10297ebb79f89b1c584fdf197401
mithem <41842729+mithem@users.noreply.github.com>
2021-12-21 15:29:45 +0100

    Update constants.php

--- constants.php
+++ constants.php
@@ -5,6 +5,7 @@ $DB_PASSWORD = "";
 $DB_HOST = "localhost";
 $READINGS_TABLE = "messwerte";
 $SCHOOL_MONITOR_TABLE = "schulmonitor";
+$SUN_HOURS_TRESHOLD = 3000; // LUX
 
 $CONNECTED_DB = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 ?>
