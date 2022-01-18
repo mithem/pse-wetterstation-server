c5fd33af50c951c0a195677bbccd6d1725eed2c0
mithem <41842729+mithem@users.noreply.github.com>
2022-01-18 14:07:09 +0100

    Add id column

--- datenabanksetup.sql
+++ datenabanksetup.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE IF NOT EXISTS wetterstation;
-CREATE TABLE IF NOT EXISTS messwerte (temperatur double, niederschlag int, lichtstaerke double, feuchtigkeit double, wind double, druck double, zeit TIMESTAMP);
+CREATE TABLE IF NOT EXISTS messwerte (id int NOT NULL AUTO_INCREMENT, temperatur double, niederschlag int, lichtstaerke double, feuchtigkeit double, wind double, druck double, zeit TIMESTAMP, PRIMARY KEY (id));
